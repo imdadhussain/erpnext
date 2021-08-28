90d3f6142ed19dace8556afebcc3e5c5d36725e3
vishdha <vishaldhayagude07@gmail.com>
2021-08-28 14:26:47 +0530

    fix: sync now

--- erpnext/erpnext_integrations/doctype/plaid_settings/plaid_settings.py
+++ erpnext/erpnext_integrations/doctype/plaid_settings/plaid_settings.py
@@ -258,14 +258,21 @@ def new_bank_transaction(transaction):
 def automatic_synchronization():
 	settings = frappe.get_doc("Plaid Settings", "Plaid Settings")
 	if settings.enabled == 1 and settings.automatic_sync == 1:
-		plaid_accounts = frappe.get_all("Bank Account", filters={"integration_id": ["!=", ""]}, fields=["name", "bank"])
+		enqueue_synchronization()
 
-		for plaid_account in plaid_accounts:
-			frappe.enqueue(
-				"erpnext.erpnext_integrations.doctype.plaid_settings.plaid_settings.sync_transactions",
-				bank=plaid_account.bank,
-				bank_account=plaid_account.name
-			)
+
+@frappe.whitelist()
+def enqueue_synchronization():
+	plaid_accounts = frappe.get_all("Bank Account",
+		filters={"integration_id": ["!=", ""]},
+		fields=["name", "bank"])
+
+	for plaid_account in plaid_accounts:
+		frappe.enqueue(
+			"erpnext.erpnext_integrations.doctype.plaid_settings.plaid_settings.sync_transactions",
+			bank=plaid_account.bank,
+			bank_account=plaid_account.name
+		)
 
 @frappe.whitelist()
 def get_link_token_for_update(access_token):
