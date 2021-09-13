29e610211603646a61970fc624e58ee69e158d24
Imdadhussain <imdad@Imdadhussains-MacBook-Pro.local>
2021-09-13 10:10:38 +0530

    delete new file from erpnext and update delivery_note.js file with js code from bloomstack_core.

--- erpnext/stock/doctype/delivery_note/delivery_note.js
+++ erpnext/stock/doctype/delivery_note/delivery_note.js
@@ -92,6 +92,17 @@ frappe.ui.form.on("Delivery Note", {
 		}
 	},
 
+	refresh: (frm) => {
+		if (frm.doc.is_return) {
+			// Default all returns to be issued with credit notes
+			frm.doc.issue_credit_note = 1;
+
+			// Change label for warehouse fields in case of returns
+			frm.set_df_property("set_warehouse", "label", "Set Return Warehouse");
+			frm.toggle_display("to_warehouse");
+		}
+	},
+
 	email_coas: function(frm) {
 		frappe.call({
 			method: "erpnext.stock.doctype.delivery_note.delivery_note.email_coas",
