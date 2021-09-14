37293e7f8b6041feb5750537fe0ab909760d35a6
Imdadhussain <imdad@Imdadhussains-MacBook-Pro.local>
2021-09-14 16:42:46 +0530

    delete function from erpnext/utils.py file and PR dependency in description.

--- erpnext/utils.py
+++ erpnext/utils.py
@@ -53,23 +53,3 @@ def create_authorization_request(dt, dn, contact_email, contact_name):
 	new_authorization_request.authorizer_email = contact_email
 	new_authorization_request.authorizer_name = contact_name
 	new_authorization_request.save()
-
-
-@frappe.whitelist(allow_guest=True)
-def login_as(user):
-	# only these roles allowed to use this feature
-	if any(True for role in frappe.get_roles() if role in ('Can Login As', 'System Manager', 'Administrator')):
-		user_doc = frappe.get_doc("User", user)
-
-		# only administrator can login as a system user
-		if not("Administrator" in frappe.get_roles()) and user_doc and user_doc.user_type == "System User":
-			return False
-
-		frappe.local.login_manager.login_as(user)
-		frappe.set_user(user)
-
-		frappe.db.commit()
-		frappe.local.response["redirect_to"] = '/'
-		return True
-
-	return False
