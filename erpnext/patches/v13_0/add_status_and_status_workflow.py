68514634b3605f5b96bf2a14ee06dcc3027a0a70
imdadhussain <imdadhussain530@gmail.com>
2021-11-11 17:47:00 +0530

    update changes in status.json and in patch file of add_status_and_status_workflow

--- erpnext/patches/v13_0/add_status_and_status_workflow.py
+++ erpnext/patches/v13_0/add_status_and_status_workflow.py
@@ -3,8 +3,8 @@ import frappe, json
 def execute():
 	from erpnext.setup.setup_wizard.operations.install_fixtures import add_status_data, add_status_workflow_data
 
-	frappe.reload_doc("setup", "doctype", "Status")
-	frappe.reload_doc("setup", "doctype", "Status Workflow")
+	frappe.reload_doc("core", "doctype", "Status", force=True)
+	frappe.reload_doc("core", "doctype", "Status Workflow", force=True)
 
 	add_status_data()
 	add_status_workflow_data()
